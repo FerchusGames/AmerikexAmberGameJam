1d8a75fd01037a936fb47842a75c7106181100ab
Ferchus <zap408@amerike.edu.mx>
2021-09-16 16:13:24 -0500

    Revert "Metodo de prendas"
    This reverts commit f4a78c4fc43e18814a9a8e937eaecf798b0ee49c.

--- Assets/Scripts/SistemaCambios.cs
+++ Assets/Scripts/SistemaCambios.cs
@@ -4,54 +4,15 @@ using UnityEngine;
 
 public class SistemaCambios : MonoBehaviour
 {
-    public GameObject[] sombreros;
-    public GameObject[] playeras;
-    public GameObject[] pantalones;
-    public GameObject[] zapatos;
-    public int sombreroSeleccionado = 0;
-    public int playeraSeleccionada = 0;
-    public int pantalonSeleccionado = 0;
-    public int zapatoSeleccionado = 0;
-
-    public void SiguienteSombrero()
+    // Start is called before the first frame update
+    void Start()
     {
-        sombreros[sombreroSeleccionado].SetActive(false);
-        sombreroSeleccionado = (sombreroSeleccionado++) % sombreros.Length;
-        sombreros[sombreroSeleccionado].SetActive(true);
+        
     }
 
-    public void AnteriorSombrero()
+    // Update is called once per frame
+    void Update()
     {
-        sombreros[sombreroSeleccionado].SetActive(false);
-        sombreroSeleccionado--;
-        if (sombreroSeleccionado < 0)
-        {
-            sombreroSeleccionado += sombreros.Length;
-        }
-        sombreros[sombreroSeleccionado].SetActive(true);
+        
     }
-
-    public void SiguientePlayera()
-    {
-        playeras[playeraSeleccionada].SetActive(false);
-        playeraSeleccionada = (playeraSeleccionada++) % playeras.Length;
-        playeras[playeraSeleccionada].SetActive(true);
-    }
-
-    public void AnteriorPlayera()
-    {
-        playeras[playeraSeleccionada].SetActive(false);
-        playeraSeleccionada--;
-        if (playeraSeleccionada < 0)
-        {
-            playeraSeleccionada += playeras.Length;
-        }
-        playeras[playeraSeleccionada].SetActive(true);
-    }
-
-    public void SiguientePrenda(GameObject[] prenda, int prendaSeleccionada)
-    {
-
-    }
-
 }
