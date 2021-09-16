0ce1097b895039b87073269c9971813a3389092e
Zap408 <zap408@amerike.edu.mx>
2021-09-16 13:33:17 -0500

    Scripts innecesarios
    Eliminé scripts innecesarios que traje de otro proyecto

--- Assets/Scripts/SistemaCambios.cs
+++ Assets/Scripts/SistemaCambios.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestroy : MonoBehaviour
+public class SistemaCambios : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        
     }
 
     // Update is called once per frame
